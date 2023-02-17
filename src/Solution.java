9bbe47b36911175771fbe2039e790da66cf02358
peiyiding <616256541@qq.com>
2023-02-17 19:16:16 +0800

    modify code

---
+++ src/Solution.java
@@ -0,0 +1,49 @@
+package src;
+
+import java.util.Scanner;
+
+/**
+ * 随机题目
+ */
+public class Solution {
+	/**
+	 * main
+	 * 
+	 * @param args 标准入参
+	 */
+	public static void main(String[] args) {
+//		test();
+		randomPractice();
+	}
+	
+	/**
+	 * 随机练习题目
+	 */
+	public static void randomPractice() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("一共学习了几节课: ");
+		int iCalss = sc.nextInt();
+		
+		System.out.println("你今天要练习的课是: " + ((int) (Math.random() * iCalss) + 1));
+		
+		System.out.print("一节课有几道题: ");
+		int iCode = sc.nextInt();
+		System.out.println("你今天要做的题目是: " + ((int) (Math.random() * iCode) + 1));
+	}
+	
+	/**
+	 * 对数器
+	 */
+	public static void test() {
+		int max = 100;
+		int testTimes = 100000;
+		
+		System.out.println("start");
+		
+		for (int i = 0; i < testTimes; i++) {
+			
+		}
+		
+		System.out.println("end");
+	}
+}
