bfd9e647b47cb5f60ca6931498cb2ef553177e6d
peiyiding <616256541@qq.com>
2023-02-20 23:14:48 +0800

    modify code

--- src/class04/Code04_BiggerThanRightTwice.java
+++ src/class04/Code04_BiggerThanRightTwice.java
@@ -1,21 +1,27 @@
 package src.class04;
 
 /**
- * 在一个数组中，任何一个前面的数a，和任何一个后面的数b，如果(a,b)是降序的，就称为降序对
- * 给定一个数组arr，求数组的降序对总数量
+ * 在一个数组中，对于任何一个数num，求有多少个(后面的数*2)依然<num，返回总个数
+ * 比如：[3,1,7,0,2] 
+ * 3的后面有：1，0 
+ * 1的后面有：0
+ * 7的后面有：0，2 
+ * 0的后面没有 
+ * 2的后面没有 
+ * 所以总共有5个
  * 
  * @author peiyiding
- * 
+ *
  */
-public class Code03_ReversePair {
-
+public class Code04_BiggerThanRightTwice {
+	
 	/**
-	 * 求数组的降序对总数量
+	 * 在一个数组中，对于任何一个数num，求有多少个(后面的数*2)依然<num，返回总个数
 	 * 
 	 * @param arr 数组
-	 * @return int 降序对数量
+ 	 * @return int 返回值
 	 */
-	public static int reversePair(int[] arr) {
+	public static int solution(int[] arr) {
 		if (arr == null || arr.length < 2) {
 			return 0;
 		}
@@ -24,7 +30,7 @@ public class Code03_ReversePair {
 	}
 	
 	/**
-	 * 递归方法
+	 * 递归求解
 	 * 
 	 * @param arr 数组
 	 * @param left 下标
@@ -37,14 +43,15 @@ public class Code03_ReversePair {
 		}
 		
 		int mid = left + ((right - left) >> 1);
-		return process(arr, left, mid) +
+		
+		return process(arr, left, mid) + 
 				process(arr, mid + 1, right) +
-				merge01(arr, left, mid, right);
+				merge(arr, left, mid, right);
 	}
 	
 	/**
-	 * 归并方法并求逆序对数量
-	 *  
+	 * 归并方法求
+	 * 
 	 * @param arr 数组
 	 * @param left 下标
 	 * @param mid 下标
@@ -52,23 +59,31 @@ public class Code03_ReversePair {
 	 * @return int 返回值
 	 */
 	public static int merge(int[] arr, int left, int mid, int right) {
-		int[] help = new int[right - left + 1];
-		int i = help.length - 1;
-		int p1 = mid;
-		int p2 = right;
+		int r = mid + 1;
 		int ans = 0;
 		
-		while (p1 >= left && p2 > mid) {
-			ans += arr[p2] < arr[p1] ? (p2 - mid) : 0;
- 			help[i--] = arr[p2] < arr[p1] ? arr[p1--] : arr[p2--];
+		for (int i = left; i <= mid; i++) {
+			while (r <= right && arr[i] > (arr[r] << 1)) {
+				r++;
+			}
+			ans += r - 1 - mid;
 		}
 		
-		while (p1 >= left) {
-			help[i--] = arr[p1--];
+		int[] help = new int[right - left + 1];
+		int i = 0;
+		int p1 = left;
+		int p2 = mid + 1;
+		
+		while (p1 <= mid && p2 <= right) {
+			help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
 		}
 		
-		while (p2 > mid) {
-			help[i--] = arr[p2--];
+		while (p1 <= mid) {
+			help[i++] = arr[p1++];
+		}
+		
+		while (p2 <= right) {
+			help[i++] = arr[p2++];
 		}
 		
 		for (i = 0; i < help.length; i++) {
@@ -81,9 +96,9 @@ public class Code03_ReversePair {
 	/**
 	 * 生成随机数组
 	 * 
-	 * @param maxLength 数组最大长度
-	 * @param maxValue 数组最大值
-	 * @return int[] 随机数组
+	 * @param maxLength 最大长度
+	 * @param maxValue 最大值
+	 * @return int 返回值
 	 */
 	public static int[] generateRandomArray(int maxLength, int maxValue) {
 		int[] arr = new int[(int) (Math.random() * maxLength)];
@@ -93,13 +108,13 @@ public class Code03_ReversePair {
 		}
 		
 		return arr;
-	}
+ 	}
 	
 	/**
 	 * 拷贝数组
 	 * 
 	 * @param arr 数组
-	 * @return int[] 随机数组
+	 * @return int 返回值
 	 */
 	public static int[] copyArray(int[] arr) {
 		int[] ans = new int[arr.length];
@@ -112,7 +127,7 @@ public class Code03_ReversePair {
 	}
 	
 	/**
-	 * 对数器测试方法
+	 * 测试方法
 	 * 
 	 * @param arr 数组
 	 * @return int 返回值
@@ -126,7 +141,9 @@ public class Code03_ReversePair {
 		
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
-				ans += arr[i] > arr[j] ? 1 : 0;
+				if (arr[i] > (arr[j] << 1)) {
+					ans += 1;
+				}
 			}
 		}
 		
@@ -138,17 +155,17 @@ public class Code03_ReversePair {
 	 */
 	public static void test() {
 		int maxLength = 100;
-		int maxValue = 100;
+		int maxValue = 1000;
 		int testTimes = 1000;
 		
 		System.out.println("start");
 		
 		for (int i = 0; i < testTimes; i++) {
 			int[] arr = generateRandomArray(maxLength, maxValue);
-			int[] testArr = copyArray(arr);
+			int[] copyArray = copyArray(arr);
 			
-			int ans1 = reversePair(arr);
-			int ans2 = testMethod(testArr);
+			int ans1 = solution(arr);
+			int ans2 = testMethod(copyArray);
 			
 			if (ans1 != ans2) {
 				System.out.println("Oops");
@@ -168,24 +185,33 @@ public class Code03_ReversePair {
 		test();
 	}
 	
-	public static int merge01(int[] arr, int left, int mid, int right) {
-		int[] help = new int[right - left + 1];
-		int i = help.length - 1;
-		int p1 = mid;
-		int p2 = right;
+	public static int merge01(int[] arr, int left, int mid, int right)  {
+		// 先处理数值
 		int ans = 0;
-	
-		while (p1 >= left && p2 > mid) {
-			ans += arr[p1] > arr[p2] ? p2 - mid : 0;
-			help[i--] = arr[p2] >= arr[p1] ? arr[p2--] : arr[p1--];
+		int r = mid + 1;
+		
+		for (int i = left; i <= mid; i++) {
+			while (r <= right && arr[i] > (arr[r] << 1)) {
+				r++;
+			}
+			ans += r - 1 - mid;
 		}
 		
-		while (p1 >= left) {
-			help[i--] = arr[p1--];
+		int[] help = new int[right - left + 1];
+		int i = 0;
+		int p1 = left;
+		int p2 = mid + 1;
+		
+		while (p1 <= mid && p2 <= right) {
+			help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
 		}
 		
-		while (p2 > mid) {
-			help[i--] = arr[p2--];
+		while (p1 <= mid) {
+			help[i++] = arr[p1++];
+		}
+		
+		while (p2 <= right) {
+			help[i++] = arr[p2++];
 		}
 		
 		for (i = 0; i < help.length; i++) {
