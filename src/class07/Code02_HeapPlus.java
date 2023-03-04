e9cdbaafb3f0f5cce3f15d2fbf11f425491db1c5
peiyiding <616256541@qq.com>
2023-03-04 23:03:16 +0800

    modify code

--- src/class07/Code02_HeapPlus.java
+++ src/class07/Code02_HeapPlus.java
@@ -17,20 +17,20 @@ public class Code02_HeapPlus<T> {
 	 * 内部包装类, 包装Integer整形, 防止map, put的时候按值传递, key会丢失
 	 * @param <T> 泛型
 	 */
-	public static class Inner<T> {
-		private T val;
-		
-		public Inner(T value) {
-			val = value;
-		}
-	}
+//	public static class Inner<T> {
+//		private T val;
+//		
+//		public Inner(T value) {
+//			val = value;
+//		}
+//	}
 	
-	private ArrayList<Inner<T>> heap;
-	private HashMap<Inner<T>, Integer> indexMap;
+	private ArrayList<T> heap;
+	private HashMap<T, Integer> indexMap;
 	private int heapSize;
-	private Comparator<? super Inner<T>> comparator;
+	private Comparator<? super T> comparator;
 	
-	public Code02_HeapPlus(Comparator<? super Inner<T>> comparator) {
+	public Code02_HeapPlus(Comparator<? super T> comparator) {
 		heap = new ArrayList<>();
 		indexMap = new HashMap<>();
 		heapSize = 0;
@@ -45,22 +45,22 @@ public class Code02_HeapPlus<T> {
 		return heapSize;
 	}
 	
-	public boolean contains(Inner<T> obj) {
+	public boolean contains(T obj) {
 		return indexMap.containsKey(obj);
 	}
 	
-	public Inner<T> peek() {
+	public T peek() {
 		return heap.get(0);
 	}
 	
-	public void push(Inner<T> obj) {
+	public void push(T obj) {
 		heap.add(obj);
 		indexMap.put(obj, heapSize);
 		heapInsert(heapSize++);
 	}
 	
-	public Inner<T> pop() {
-		Inner<T> ans = heap.get(0);
+	public T pop() {
+		T ans = heap.get(0);
 		swap(0, heapSize - 1);
 		indexMap.remove(ans);
 		heap.remove(--heapSize);
@@ -68,8 +68,8 @@ public class Code02_HeapPlus<T> {
 		return ans;
 	}
 	
-	public void remove(Inner<T> obj) {
-		Inner<T> replace = heap.get(heapSize - 1);
+	public void remove(T obj) {
+		T replace = heap.get(heapSize - 1);
 		int index = indexMap.get(obj);
 		indexMap.remove(obj);
 		heap.remove(--heapSize);
@@ -81,15 +81,15 @@ public class Code02_HeapPlus<T> {
 		}
 	}
 	
-	public void resign(Inner<T> obj) {
+	public void resign(T obj) {
 		heapInsert(indexMap.get(obj));
 		heapify(indexMap.get(obj));
 	}
 	
-	public List<Inner<T>> getAllElements() {
-		List<Inner<T>> ans = new ArrayList<>();
+	public List<T> getAllElements() {
+		List<T> ans = new ArrayList<>();
 		
-		for (Inner<T> element : heap) {
+		for (T element : heap) {
 			ans.add(element);
 		}
 		
@@ -136,8 +136,8 @@ public class Code02_HeapPlus<T> {
 	 * @param j 下标
 	 */
 	public void swap(int i, int j) {
-		Inner<T> o1 = heap.get(i);
-		Inner<T> o2 = heap.get(j);
+		T o1 = heap.get(i);
+		T o2 = heap.get(j);
 		
 		heap.set(i, o2);
 		heap.set(j, o1);
