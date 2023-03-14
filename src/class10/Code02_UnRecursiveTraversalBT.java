87b382d14ede6cfa9725f92da9a5533c8ce31b30
peiyiding <peiyiding@weimeng-hosp.com>
2023-03-14 10:17:42 +0800

    modify code

--- src/class10/Code02_UnRecursiveTraversalBT.java
+++ src/class10/Code02_UnRecursiveTraversalBT.java
@@ -6,17 +6,99 @@ import java.util.Stack;
  * 二叉树先序、中序、后序的非递归遍历
  */
 public class Code02_UnRecursiveTraversalBT {
-	
+
 	public static class Node {
 		public int value;
 		public Node left;
 		public Node right;
-		
+
 		public Node(int v) {
 			value = v;
 		}
 	}
+
+	/**
+	 * 先序遍历
+	 * 
+	 * @param head 头结点
+	 */
+	public static void pre(Node head) {
+		System.out.print("pre-order: ");
+		if (head != null) {
+			Stack<Node> stack = new Stack<>();
+			stack.push(head);
+			while (!stack.isEmpty()) {
+				head = stack.pop();
+				System.out.print(head.value + " ");
+				
+				if (head.right != null) {
+					stack.push(head.right);	
+				}
+				
+				if (head.left != null) {
+					stack.push(head.left);	
+				}
+			}
+
+		}
+	}
 	
+	/**
+	 * 中序遍历
+	 * 
+	 * @param head 头结点
+	 */
+	public static void in(Node head) {
+		System.out.print("in-order: ");
+		if (head != null) {
+			Stack<Node> stack = new Stack<>();
+			while (!stack.isEmpty() || head != null) {
+				if (head != null) {
+					stack.push(head);
+					head = head.left;
+				} else {
+					head = stack.pop();
+					System.out.print(head.value + " ");
+					head = head.right;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 使用两个栈空间
+	 * 
+	 * @param head 头结点
+	 */
+	public static void pos1(Node head) {
+		System.out.print("pos-order: ");
+		if (head != null) {
+			Stack<Node> stack = new Stack<>();
+			Stack<Node> popStack = new Stack<>();
+			stack.push(head);
+			while (!stack.isEmpty()) {
+				head = stack.pop();
+				popStack.push(head);
+				if (head.left != null) {
+					stack.push(head.left);
+				}
+				
+				if (head.right != null) {
+					stack.push(head.right);
+				}
+			}
+			
+			while (!popStack.isEmpty()) {
+				System.out.print(popStack.pop().value + " ");
+			}
+		}
+	}
+
+	/**
+	 * 使用一个栈空间
+	 * 
+	 * @param h 头结点
+	 */
 	public static void pos2(Node h) {
 		System.out.print("pos-order: ");
 		if (h != null) {
@@ -38,6 +120,11 @@ public class Code02_UnRecursiveTraversalBT {
 		System.out.println();
 	}
 
+	/**
+	 * main方法
+	 * 
+	 * @param args 标准入参
+	 */
 	public static void main(String[] args) {
 		Node head = new Node(1);
 		head.left = new Node(2);
