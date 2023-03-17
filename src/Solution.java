89c44d58e0c84fde626b198f1825cd568fb81e9c
peiyiding <peiyiding@weimeng-hosp.com>
2023-03-17 16:52:41 +0800

    modify code

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,28 @@
 package src;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
 	/**
-	 * 随机复习
+	 * Randomly generate a question number from the total number of questions learned
 	 */
-	public static void main(String[] args) {
+	private static void randomQuestionNo() {
 		try (Scanner sc = new Scanner(System.in)) {			
 			System.out.print("一共学习了几道题: ");
 			System.out.print("你今天要做的题目是: " + ((int) (Math.random() * sc.nextInt()) + 1));
 		}
 	}
+
+	/**
+	 * main
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		randomQuestionNo();
+	}
+
+	// Talk is cheap.Show me your code
+	// TODO
+	
 }
