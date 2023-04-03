b791cd54ae6bf3560bef5ccb8e35474d23884c3d
peiyiding <616256541@qq.com>
2023-04-03 08:15:59 +0800

    modify code

--- src/class14/Code04_Light.java
+++ src/class14/Code04_Light.java
@@ -130,7 +130,7 @@ public class Code04_Light {
 			if (ans1 != ans2
 				|| ans2 != ans3) {
 				System.out.println("str = " + str);
-				System.out.println("ans1 = " + ans1 + ", ans2 = " + ans2 + "ans3 = " + ans3);
+				System.out.println("ans1 = " + ans1 + ", ans2 = " + ans2 + ", ans3 = " + ans3);
 				System.out.println("Oops");
 				break;
 			}
