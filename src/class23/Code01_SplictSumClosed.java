7ecb2f6fd19c3a3bd0b404fa39d54fa20b06e8ca
peiyiding <616256541@qq.com>
2023-05-07 16:16:50 +0800

    modify code

--- src/class23/Code01_SplictSumClosed.java
+++ src/class23/Code01_SplictSumClosed.java
@@ -9,12 +9,17 @@ import src.Solution;
  */
 public class Code01_SplictSumClosed {
 	// solution one
-	public static int right(int[] arr, int num) {
+	public static int right(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
 		
-		return process(arr, 0, num / 2);
+		int sum = 0;
+		for (int num : arr) {
+			sum += num;
+		}
+		
+		return process(arr, 0, sum / 2);
 	}
 	
 	// recursive method one
@@ -34,17 +39,22 @@ public class Code01_SplictSumClosed {
 	}
 	
 	// solution two
-	public static int dp(int[] arr, int num) {
+	public static int dp(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
 		
-		num /= 2;
+		int sum = 0;
+		for (int num : arr) {
+			sum += num;
+		}
+		
+		sum /= 2;
 		int N = arr.length;
-		int[][] dp = new int[N + 1][num + 1];
+		int[][] dp = new int[N + 1][sum + 1];
 		
 		for (int i = N - 1; i >= 0; i--) {
-			for (int rest = 0; rest <= num; rest++) {
+			for (int rest = 0; rest <= sum; rest++) {
 				int p1 = dp[i + 1][rest];
 				
 				int p2 = 0;
@@ -56,7 +66,7 @@ public class Code01_SplictSumClosed {
 			}
 		}
 		
-		return dp[0][num];
+		return dp[0][sum];
 	}
 
 	// for test
@@ -73,21 +83,19 @@ public class Code01_SplictSumClosed {
 	
 	// for test
 	public static void test() {
-		int len = 50;
-		int max = 100;
+		int len = 20;
+		int max = 50;
 		int testTimes = 1000;
 		
 		System.out.println("start");
 		
 		for (int i = 0; i < testTimes; i++) {
 			int[] arr = generateRandomArray(len, max);
-			int num = (int) (Math.random() * max) + 1;
-			
-			int ans1 = right(arr, num);
-			int ans2 = dp(arr, num);
+
+			int ans1 = right(arr);
+			int ans2 = dp(arr);
 			
 			if (ans1 != ans2) {
-				System.out.println("num = " + num);
 				Solution.printArray(arr);
 				System.out.println("ans1 = " + ans2 + ", ans2 = " + ans2);
 				System.out.println("Oops");
