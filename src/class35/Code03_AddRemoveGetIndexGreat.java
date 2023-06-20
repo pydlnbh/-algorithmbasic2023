d2942a0fc61707c0726ab20e7efec74fba458fc4
peiyiding <peiyiding@weimeng-hosp.com>
2023-06-20 10:04:23 +0800

    modify code

--- src/class35/Code03_AddRemoveGetIndexGreat.java
+++ src/class35/Code03_AddRemoveGetIndexGreat.java
@@ -2,6 +2,13 @@ package src.class35;
 
 import java.util.ArrayList;
 
+/**
+ * 设计一个结构包含如下两个方法：
+ * void add(int index, int num)：把num加入到index位置
+ * int get(int index) ：取出index位置的值
+ * void remove(int index) ：把index位置上的值删除
+ * 要求三个方法时间复杂度O(logN)
+ */
 public class Code03_AddRemoveGetIndexGreat {
 	// auxiliary class
 	public static class SBTNode<V> {
