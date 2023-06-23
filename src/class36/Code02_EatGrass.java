7b9b99f314ac6cd8e848be36427a28c25cc9869e
peiyiding <616256541@qq.com>
2023-06-23 23:19:41 +0800

    modify code

---
+++ src/class36/Code02_EatGrass.java
@@ -0,0 +1,46 @@
+package src.class36;
+
+/**
+ * 给定一个正整数N，表示有N份青草统一堆放在仓库里，有一只牛和一只羊，牛先吃，羊后吃，它俩轮流吃草
+ * 不管是牛还是羊，每一轮能吃的草量必须是：1，4，16，64…(4的某次方)
+ * 谁最先把草吃完，谁获胜，假设牛和羊都绝顶聪明，都想赢，都会做出理性的决定。根据唯一的参数N，返回谁会赢
+ */
+public class Code02_EatGrass {
+	// solution one
+	public static String whoWin(int n) {
+		if (n < 5) {
+			return n == 0 || n == 2 ? "后手" : "先手";
+		}
+		
+		int want = 1;
+		while (want <= n) {
+			if (whoWin(n - want).equals("后手")) {
+				return "先手";
+			}
+			
+			if (want <= (n / 4)) {
+				want *= 4;
+			} else {
+				break;
+			}
+		}
+		
+		return "后手";
+	}
+	
+	// solution two
+	public static String winner2(int n) {
+		if (n % 5 == 0 || n % 5 == 2) {
+			return "后手";
+		} else {
+			return "先手";
+		}
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 50; i++) {
+			System.out.println(i + " : " + whoWin(i));
+		}
+	}
+
+}
