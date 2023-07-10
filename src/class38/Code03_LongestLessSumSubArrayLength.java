71fbd9c13968540954f118009ef9700ef1c8cef0
peiyiding <peiyiding@weimeng-hosp.com>
2023-07-10 11:57:36 +0800

    modify code

---
+++ src/class38/Code03_LongestLessSumSubArrayLength.java
@@ -0,0 +1,129 @@
+package src.class38;
+
+/**
+ * 给定一个整数组成的无序数组arr，值可能正、可能负、可能0，给定一个整数值K
+ * 找到arr的所有子数组里，哪个子数组的累加和<=K，并且是长度最大的，返回其长度
+ */
+public class Code03_LongestLessSumSubArrayLength {
+	// solution one
+	public static int maxLengthAwesome(int[] arr, int k) {
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		
+		int[] minSums = new int[arr.length];
+		int[] minSumEnds = new int[arr.length];
+		minSums[arr.length - 1] = arr[arr.length - 1];
+		minSumEnds[arr.length - 1] = arr.length - 1;
+		
+		for (int i = arr.length - 2; i >= 0; i--) {
+			if (minSums[i + 1] < 0) {
+				minSums[i] = arr[i] + minSums[i + 1];
+				minSumEnds[i] = minSumEnds[i + 1];
+			} else {
+				minSums[i] = arr[i];
+				minSumEnds[i] = i;
+			}
+		}
+		
+		int end = 0;
+		int sum = 0;
+		int ans = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			while (end < arr.length && sum + minSums[end] <= k) {
+				sum += minSums[end];
+				end = minSumEnds[end] + 1;
+			}
+			
+			ans = Math.max(ans, end - i);
+			
+			if (end > i) {
+				sum -= arr[i];
+			} else {
+				end = i + 1;
+			}
+		}
+		
+		return ans;
+	}
+	
+	// solution two
+	public static int maxLength(int[] arr, int k) {
+		int[] h = new int[arr.length + 1];
+		int sum = 0;
+		h[0] = sum;
+		for (int i = 0; i != arr.length; i++) {
+			sum += arr[i];
+			h[i + 1] = Math.max(sum, h[i]);
+		}
+		
+		sum = 0;
+		int res = 0;
+		int pre = 0;
+		int len = 0;
+		
+		for (int i = 0; i != arr.length; i++) {
+			sum += arr[i];
+			pre = getLessIndex(h, sum - k);
+			len = pre == -1 ? 0 : i - pre + 1;
+			res = Math.max(res, len);
+		}
+		
+		return res;
+	}
+	
+	public static int getLessIndex(int[] arr, int num) {
+		int low = 0;
+		int high = arr.length - 1;
+		int mid = 0;
+		int res = -1;
+		
+		while (low <= high) {
+			mid = (low + high) / 2;
+			if (arr[mid] >= num) {
+				res = mid;
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		
+		return res;
+	}
+	
+	// for test
+	public static int[] generateRandomArray(int len, int maxValue) {
+		int[] res = new int[len];
+		for (int i = 0; i < res.length; i++) {
+			res[i] = (int) (Math.random() * maxValue) - (maxValue / 3);
+		}
+		return res;
+	}
+	
+	// for test
+	public static void test() {
+		int len = 10;
+		int maxValue = 20;
+		int testTimes = 1000000;
+		
+		System.out.println("start");
+		
+		for (int i = 0; i < testTimes; i++) {
+			int[] arr = generateRandomArray(len, maxValue);
+			int k = (int) (Math.random() * maxValue) - 5;
+			
+			if (maxLengthAwesome(arr, k) != maxLength(arr, k)) {
+				System.out.println("Oops");
+				break;
+			}
+		}
+		
+		System.out.println("end");
+	}
+	
+	// main
+	public static void main(String[] args) {
+		test();
+	}
+}
