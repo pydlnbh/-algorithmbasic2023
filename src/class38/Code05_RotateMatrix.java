67a7742fa71d9f0b270d9483613e10dc05534afe
peiyiding <peiyiding@weimeng-hosp.com>
2023-07-14 16:30:17 +0800

    modify code

---
+++ src/class38/Code05_RotateMatrix.java
@@ -0,0 +1,46 @@
+package src.class38;
+
+// 给定一个正方形矩阵matrix，原地调整成顺时针90度转动的样子
+public class Code05_RotateMatrix {
+	// solution one
+	public static void rotate(int[][] matrix) {
+		int a = 0;
+		int b = 0;
+		int c = matrix.length - 1;
+		int d = matrix[0].length - 1;
+		
+		while (a < c) {
+			rotateEdge(matrix, a++, b++, c--, d--);
+		}
+	}
+	
+	public static void rotateEdge(int[][] matrix, int a, int b, int c, int d) {
+		int tmp = 0;
+		
+		for (int i = 0; i < d - b; i++) {
+			tmp = matrix[a][b + i];
+			matrix[a][b + i] = matrix[c - i][b];
+			matrix[c - i][b] = matrix[c][d - i];
+			matrix[c][d - i] = matrix[a + i][d];
+			matrix[a + i][d] = tmp;
+		}
+	}
+	
+	public static void printMatrix(int[][] matrix) {
+		for (int i = 0; i != matrix.length; i++) {
+			for (int j = 0; j != matrix[0].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	// main
+	public static void main(String[] args) {
+		int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+		printMatrix(matrix);
+		rotate(matrix);
+		System.out.println("=====================");
+		printMatrix(matrix);
+	}
+}
